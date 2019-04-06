bc76b56c0f433b4d28899c0a25da35aac4136367
Rad <chihaby@gmail.com>
2019-04-06 11:21:28 -0700

    functionality complete

--- routes/apiRoutes.js
+++ routes/apiRoutes.js
@@ -3,27 +3,10 @@ var db = require("../models");
 module.exports = function (app) {
   // Get all examples
   app.get("/api/examples", function (req, res) {
-    db.Example3.findAll({}).then(function (dbExamples) {
+    db.Example3.findAll(
+      { limit: 10, order: [['like', 'DESC']] }
+    ).then(function (dbExamples) {
       res.json(dbExamples);
-
-      console.log(JSON.stringify(dbExamples[0]));
-      var sortedArray = [];
-      dbExamples.forEach(element => {
-        sortedArray.push(element.like);
-        var popular = sortedArray.sort(function (a, b) { return b - a });
-        console.log({ sortedArray });
-        for (var i = 0; i < dbExamples.length; i++) {
-          if (dbExamples[i].like === sortedArray[2]) {
-            console.log("id: " + dbExamples[i].id + " - Name: " + dbExamples[i].text + " - descrition: " + dbExamples[i].description + " - like: " + dbExamples[i].like);
-          }
-          if (dbExamples[i].like === sortedArray[1]) {
-            console.log("id: " + dbExamples[i].id + " - Name: " + dbExamples[i].text + " - descrition: " + dbExamples[i].description + " - like: " + dbExamples[i].like);
-          }
-          if (dbExamples[i].like === sortedArray[0]) {
-            console.log("id: " + dbExamples[i].id + " - Name: " + dbExamples[i].text + " - descrition: " + dbExamples[i].description + " - like: " + dbExamples[i].like);
-          }
-        }
-      });
     });
   });
 
