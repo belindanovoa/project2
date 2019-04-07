06f2fc08a05ec7ecb1dbed847350cb470a7045ec
Rad <chihaby@gmail.com>
2019-04-07 10:15:52 -0700

    updates

--- routes/htmlRoutes.js
+++ routes/htmlRoutes.js
@@ -3,25 +3,22 @@ var path = require("path");
 module.exports = function (app) {
   //Load index page
   app.get("/", function (req, res) {
-    db.Example3.findAll({}).then(function (results) {
-      var hbObj = {
-        Examples: results
-      }
-      res.render("index", hbObj);
+    res.render("index");
+  });
+};
+
+
+// Load example page and pass in an example by id
+app.get("/example/:id", function (req, res) {
+  db.Example3.findOne({ where: { id: req.params.id } }).then(function (dbExample) {
+    res.render("example", {
+      example: dbExample
     });
   });
+});
 
-  // Load example page and pass in an example by id
-  app.get("/example/:id", function (req, res) {
-    db.Example3.findOne({ where: { id: req.params.id } }).then(function (dbExample) {
-      res.render("example", {
-        example: dbExample
-      });
-    });
-  });
-
-  // Render 404 page for any unmatched routes
-  app.get("*", function (req, res) {
-    res.render("404");
-  });
+// Render 404 page for any unmatched routes
+app.get("*", function (req, res) {
+  res.render("404");
+});
 };
